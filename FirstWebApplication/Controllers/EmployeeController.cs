ce37ed61c4a6d9c561325f275ae5bf2d9491124e
sujina.mohanarajan <sujina.mohanarajan@gavstech.com>
2023-09-22 11:44:29 +0530

    Employee commit

---
+++ FirstWebApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using FirstWebApplication.Models;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private RepositoryEmployee _repositoryEmployee;
+        public EmployeeController(RepositoryEmployee repository)
+        {
+            _repositoryEmployee = repository;
+        }
+        [HttpGet("/GetAllEmployees")]
+        // GET: EmployeeController
+        public IEnumerable<EmpViewModel> GetAllEmployees()
+        {
+            List<Employee> employees = _repositoryEmployee.GetAllEmployees();
+            var emplist = (from emp in employees
+                           select new EmpViewModel()
+                           {
+                               EmpId = emp.EmployeeId,
+                               FirstName = emp.FirstName,
+                               LastName = emp.LastName,
+                               BirthDate = (DateTime)emp.BirthDate,
+                               HireDate = (DateTime)emp.HireDate,
+                               Title = emp.Title,
+                               City = emp.City,
+                               ReportsTo = (int)emp.ReportsTo
+                           }
+                           ).ToList();
+
+            return emplist;
+        }
+        [HttpPost]
+        public Employee EmployeeDetails(int id)
+        {
+            Employee employees = _repositoryEmployee.GetEmployeeId(id);
+            return employees;
+        }
+        [HttpPut]
+        public Employee EditEmployee(int id, [FromBody] Employee updatedEmployee)
+        {
+            updatedEmployee.EmployeeId = id;
+            Employee savedEmployee = _repositoryEmployee.UpdateEmployee(updatedEmployee);
+            return savedEmployee;
+        }
+        [HttpDelete]
+      
+            public Employee Delete(int id, [FromBody] Employee deleteEmployeeData)
+            {
+                deleteEmployeeData.EmployeeId = id;
+                Employee savedEmployee = _repositoryEmployee.DeleteEmployee(deleteEmployeeData);
+                return savedEmployee;
+            }
+        }
+    }
---
+++ FirstWebApplication/Models/RepositoryEmployee.cs
@@ -0,0 +1,50 @@
+﻿namespace FirstWebApplication.Models
+{
+    public class RepositoryEmployee
+    {
+        private NorthWindContext _context;
+        public RepositoryEmployee(NorthWindContext context)
+        {
+            _context = context;
+        }
+        public List<Employee> GetAllEmployees()
+        {
+            return _context.Employees.ToList();
+        }
+        public Employee GetEmployeeId(int id)
+        {
+            Employee employee = _context.Employees.Find(id);
+            return employee;
+        }
+        public Employee UpdateEmployee(Employee updatedEmployee)
+        {
+            //Employee employee = _context.Employees.Find(id);
+            //if (employee == null)
+            //{
+            //    throw new ArgumentException("Customer not found.");
+            //}
+            _context.Employees.Attach(updatedEmployee);
+            // Save changes to the database
+            _context.SaveChanges();
+            return updatedEmployee;
+        }
+        public int DeleteEmployee(int id)
+        {
+            //Employee employee = _context.Employees.Find(id);
+            //if (employee == null)
+            //{
+            //    throw new ArgumentException("Customer not found.");
+            //}
+            var employee = _context.Employees.Find(id);
+            _context.Employees.Remove(employee);
+            return _context.SaveChanges();
+        }
+        public Employee DeleteEmployee(Employee deleteEmployeeData)
+        {
+
+            _context.Employees.Remove(deleteEmployeeData);
+            _context.SaveChanges();
+            return deleteEmployeeData;
+        }
+    }
+}
